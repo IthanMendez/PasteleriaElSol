aaa44fb9e40c0ee65ae140613e05a8676ae3e494
Zerok01 <90226352+Zerok01@users.noreply.github.com>
2022-05-18 12:59:18 -0500

    Update pedidospendientes.php

--- pedidospendientes.php
+++ pedidospendientes.php
@@ -19,24 +19,46 @@ $conexion = $objeto->Conectar();
     <link rel="stylesheet"  type="text/css" href="datatables/DataTables-1.11.4/css/dataTables.bootstrap5.min.css">
     <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.3.1/css/all.css'>
   </head>
-  <header>
-  <nav class="navbar navbar-dark bg-dark">
-  <div class="container-fluid">
-    <a class="navbar-brand">Navbar</a>
-    <form class="d-flex input-group w-auto">
-      <input
-        type="search"
-        class="form-control rounded"
-        placeholder="Search"
-        aria-label="Search"
-        aria-describedby="search-addon"
-      />
-      <span class="input-group-text border-0" id="search-addon">
-        <i class="fas fa-search"></i>
-      </span>
-    </form>
-  </div>
-  </nav>
+  <header class="p-3 mb-3 border-bottom">
+    
+    <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start" >
+      
+    <a href="catalogo.php">
+      <img src="img/sunlogo2.png"  width="30%" height="40%" class="rounded mx-auto d-block" alt="Responsive image">
+    </a>
+    
+      <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+      
+        <li><a href="#" class="nav-link px-2 link-primary fs-4 fw-bold" >Inicio</a></li>
+        <li><a href="catalogo.php" class="nav-link px-2 link-dark fs-4 fw-bold">Catalogo</a></li>
+      
+      
+      <?php 
+
+        if(isset($_SESSION['login_user'])){
+          ?>
+          </ul>
+          <?php 
+          require_once("php/usernav.php");
+        }
+        elseif(isset($_SESSION['login_admin'])){
+          ?>
+            <a href="pedidospendientes.php" class="nav-link px-2 link-dark fs-4 fw-bold">Pedidos</a>
+            <a href="visual_Pagos.php" class="nav-link px-2 link-dark fs-4 fw-bold">Pagos</a>
+            <a href="inventario.php" class="nav-link px-2 link-dark fs-4 fw-bold">Inventario</a>
+            </ul>
+          <?php
+          require_once("php/usernav.php");
+        }
+        else{
+          ?>
+          </ul>
+          <a href="login.php" class="nav-link px-2 link-dark fs-4 fw-bold"  >Ingresar</a>
+          
+          <?php
+        }
+      ?>
+    </div>
   </header>
   <body class="min-vh-100">
   <br>
