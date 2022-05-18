46e0e0922dd050d511f6ca3a1967bf68180b50ac
Zerok01 <90226352+Zerok01@users.noreply.github.com>
2022-05-18 13:04:58 -0500

    Update categorias.php

--- categorias.php
+++ categorias.php
@@ -49,7 +49,7 @@ require_once('./php/component.php');
         elseif(isset($_SESSION['login_admin'])){
           ?>
             <a href="pedidospendientes.php" class="nav-link px-2 link-dark fs-4 fw-bold">Pedidos</a>
-            <a href="register.php" class="nav-link px-2 link-dark fs-4 fw-bold">Pagos</a>
+            <a href="visual_Pagos.php" class="nav-link px-2 link-dark fs-4 fw-bold">Pagos</a>
             <a href="inventario.php" class="nav-link px-2 link-dark fs-4 fw-bold">Inventario</a>
             </ul>
           <?php
